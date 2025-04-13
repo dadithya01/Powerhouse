0e21b6fd7f03dae9ac0ede49754420b67835e0e5
dadithya01 <dadithya01@gmail.com>
2025-04-13 21:08:48 +0530

    Update

--- src/main/java/edu/ijse/powerhouse/AppInitializer.java
+++ src/main/java/edu/ijse/powerhouse/AppInitializer.java
@@ -4,17 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        Image icon = new Image(getClass().getResourceAsStream("/images/dumbbell.png"));
+
         Parent parent = FXMLLoader.load(getClass().getResource("/view/MainPage.fxml"));
 
         Scene scene = new Scene(parent);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Powerhouse");
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(icon);
         primaryStage.show();
     }
 
