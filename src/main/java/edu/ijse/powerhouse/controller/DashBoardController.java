838fc074f5a57eac2bf0b49120c9f98d8d139333
dadithya01 <dadithya01@gmail.com>
2025-04-14 18:35:21 +0530

    Update

--- src/main/java/edu/ijse/powerhouse/controller/DashBoardController.java
+++ src/main/java/edu/ijse/powerhouse/controller/DashBoardController.java
@@ -1,4 +1,21 @@
 package edu.ijse.powerhouse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class DashBoardController {
+
+    @FXML
+    private AnchorPane ancDashBoard;
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) throws IOException {
+        ancDashBoard.getChildren().clear();
+        AnchorPane load = FXMLLoader.load(getClass().getResource("/view/MainPage.fxml"));
+        ancDashBoard.getChildren().add(load);
+    }
 }
