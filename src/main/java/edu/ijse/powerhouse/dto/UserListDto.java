881ef1b8ce904cc3309843dfa11b08a7999e8e32
dadithya01 <dadithya01@gmail.com>
2025-04-28 22:45:58 +0530

    Update

--- src/main/java/edu/ijse/powerhouse/dto/UserListDto.java
+++ src/main/java/edu/ijse/powerhouse/dto/UserListDto.java
@@ -1,8 +1,8 @@
-package edu.ijse.powerhouse.dto.tm;
+package edu.ijse.powerhouse.dto;
 
 import java.time.LocalDate;
 
-public class UserListTM {
+public class UserListDto {
     private String user_Id;
     private String name;
     private String phone;
@@ -13,11 +13,10 @@ public class UserListTM {
     private LocalDate registration_Date;
     private String status;
 
-    public UserListTM() {
+    public UserListDto() {
     }
 
-    public UserListTM(String user_Id, String name, String phone, String email, String user_Name, String password,
-            String user_Type_Id, LocalDate registration_Date, String status) {
+    public UserListDto(String user_Id, String name, String phone, String email, String user_Name, String password, String user_Type_Id,LocalDate registration_Date, String status) {
         this.user_Id = user_Id;
         this.name = name;
         this.phone = phone;
@@ -32,12 +31,15 @@ public class UserListTM {
     public String getUser_Id() {
         return user_Id;
     }
+
     public void setUser_Id(String user_Id) {
         this.user_Id = user_Id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -47,53 +49,60 @@ public class UserListTM {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getUser_Name() {
         return user_Name;
     }
+
     public void setUser_Name(String user_Name) {
         this.user_Name = user_Name;
     }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+
     public String getUser_Type_Id() {
         return user_Type_Id;
     }
+
     public void setUser_Type_Id(String user_Type_Id) {
         this.user_Type_Id = user_Type_Id;
     }
+
     public LocalDate getRegistration_Date() {
         return registration_Date;
     }
     public void setRegistration_Date(LocalDate registration_Date) {
         this.registration_Date = registration_Date;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
+
     @Override
     public String toString() {
-        return "UserListTM{" +
-                "user_Id='" + user_Id + '\'' +
-                ", name='" + name + '\'' +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", user_Name='" + user_Name + '\'' +
+        return "UserListDto{" +
+                "userId='" + user_Id + '\'' +
+                ", userName='" + user_Name + '\'' +
+                ", userType='" + user_Type_Id + '\'' +
                 ", password='" + password + '\'' +
-                ", user_Type_Id='" + user_Type_Id + '\'' +
-                ", registration_Date=" + registration_Date +
                 ", status='" + status + '\'' +
                 '}';
     }
