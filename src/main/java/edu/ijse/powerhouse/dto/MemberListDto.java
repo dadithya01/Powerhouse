d28d6d2ed08b8b14796cb550b7f634460f8eb723
dadithya01 <dadithya01@gmail.com>
2025-05-09 23:07:51 +0530

    Update

--- src/main/java/edu/ijse/powerhouse/dto/MemberListDto.java
+++ src/main/java/edu/ijse/powerhouse/dto/MemberListDto.java
@@ -1,8 +1,15 @@
 package edu.ijse.powerhouse.dto;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.sql.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class MemberListDto {
     private String member_id;
     private String name;
@@ -17,137 +24,4 @@ public class MemberListDto {
     private String membership_status;
     private String added_by;
 
-    public MemberListDto(){
-
-    }
-
-    public MemberListDto(String member_id, String name, BigDecimal weight, BigDecimal height, int age, String contact,
-                         String emergency_contact, String medical_conditions, String fitness_goals, Date register_date,
-                         String membership_status, String added_by) {
-        this.member_id = member_id;
-        this.name = name;
-        this.weight = weight;
-        this.height = height;
-        this.age = age;
-        this.contact = contact;
-        this.emergency_contact = emergency_contact;
-        this.medical_conditions = medical_conditions;
-        this.fitness_goals = fitness_goals;
-        this.register_date = register_date;
-        this.membership_status = membership_status;
-        this.added_by = added_by;
-    }
-
-    public String getMember_id() {
-        return member_id;
-    }
-
-    public void setMember_id(String member_id) {
-        this.member_id = member_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getWeight() {
-        return weight;
-    }
-
-    public void setWeight(BigDecimal weight) {
-        this.weight = weight;
-    }
-
-    public BigDecimal getHeight() {
-        return height;
-    }
-
-    public void setHeight(BigDecimal height) {
-        this.height = height;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public String getEmergency_contact() {
-        return emergency_contact;
-    }
-
-    public void setEmergency_contact(String emergency_contact) {
-        this.emergency_contact = emergency_contact;
-    }
-
-    public String getMedical_conditions() {
-        return medical_conditions;
-    }
-
-    public void setMedical_conditions(String medical_conditions) {
-        this.medical_conditions = medical_conditions;
-    }
-
-    public String getFitness_goals() {
-        return fitness_goals;
-    }
-
-    public void setFitness_goals(String fitness_goals) {
-        this.fitness_goals = fitness_goals;
-    }
-
-    public Date getRegister_date() {
-        return register_date;
-    }
-
-    public void setRegister_date(Date register_date) {
-        this.register_date = register_date;
-    }
-
-    public String getMembership_status() {
-        return membership_status;
-    }
-
-    public void setMembership_status(String membership_status) {
-        this.membership_status = membership_status;
-    }
-
-    public String getAdded_by() {
-        return added_by;
-    }
-
-    public void setAdded_by(String added_by) {
-        this.added_by = added_by;
-    }
-
-    public String toString() {
-        return "MemberListDto{" +
-                "member_id='" + member_id + '\'' +
-                ", name='" + name + '\'' +
-                ", weight=" + weight +
-                ", height=" + height +
-                ", age=" + age +
-                ", contact='" + contact + '\'' +
-                ", emergency_contact='" + emergency_contact + '\'' +
-                ", medical_conditions='" + medical_conditions + '\'' +
-                ", fitness_goals='" + fitness_goals + '\'' +
-                ", register_date=" + register_date +
-                ", membership_status='" + membership_status + '\'' +
-                ", added_by='" + added_by + '\'' +
-                '}';
-    }
 }
--- src/main/java/edu/ijse/powerhouse/dto/TrainerListDto.java
+++ src/main/java/edu/ijse/powerhouse/dto/TrainerListDto.java
@@ -5,6 +5,11 @@ import lombok.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class TrainerListDto {
     private String trainer_id;
     private String user_id;
@@ -18,126 +23,4 @@ public class TrainerListDto {
     private String bio;
     private BigDecimal rating;
 
-    public TrainerListDto() {
-    }
-
-    public TrainerListDto(String trainer_id, String user_id, String name, String contact, String address, int age,
-            String specialization, String certification, Date hire_date, String bio, BigDecimal rating) {
-        this.trainer_id = trainer_id;
-        this.user_id = user_id;
-        this.name = name;
-        this.contact = contact;
-        this.address = address;
-        this.age = age;
-        this.specialization = specialization;
-        this.certification = certification;
-        this.hire_date = hire_date;
-        this.bio = bio;
-        this.rating = rating;
-    }
-
-    public String getTrainer_id() {
-        return trainer_id;
-    }
-
-    public void setTrainer_id(String trainer_id) {
-        this.trainer_id = trainer_id;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    public String getCertification() {
-        return certification;
-    }
-
-    public void setCertification(String certification) {
-        this.certification = certification;
-    }
-
-    public Date getHire_date() {
-        return hire_date;
-    }
-
-    public void setHire_date(Date hire_date) {
-        this.hire_date = hire_date;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public BigDecimal getRating() {
-        return rating;
-    }
-
-    public void setRating(BigDecimal rating) {
-        this.rating = rating;
-    }
-
-    @Override
-    public String toString() {
-        return "TrainerListDto{" +
-                "trainer_id='" + trainer_id + '\'' +
-                ", user_id='" + user_id + '\'' +
-                ", name='" + name + '\'' +
-                ", contact='" + contact + '\'' +
-                ", address='" + address + '\'' +
-                ", age=" + age +
-                ", specialization='" + specialization + '\'' +
-                ", certification='" + certification + '\'' +
-                ", hire_date=" + hire_date +
-                ", bio='" + bio + '\'' +
-                ", rating=" + rating +
-                '}';
-    }
 }
